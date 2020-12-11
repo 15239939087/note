da1c35341578ccbba73a3662e12977460077f77b
chenwei <=>
2020-12-11 16:15:28 +0800

    feat: 全局样式配置

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-.el-table {
-  width: 100% !important;
-}
-.el-table--border th.gutter:last-of-type {
-  display: block !important;
-  width: 17px !important;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "src/themes/index";
 </style>
