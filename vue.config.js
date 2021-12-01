2156d8c9215b4245ffb776b718257b10b3922a3f
chenwei <fengzhi.chen@tuya.com>
2021-12-01 16:45:30 +0800

    feat: linkss

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const config = {
-  publicPath: "/demo",
+  publicPath: "./",
   configureWebpack: () => ({
     entry: path.resolve(__dirname, "src/main.js"),
     output: {
